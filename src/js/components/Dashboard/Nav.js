f8c0b47ccfc54992ef11672928039c5bb7c44d7a
karatechops <contact@lazers.tv>
2017-01-09 18:16:35 -0500

    Update Dashboard nav styling.

--- src/js/components/Dashboard/Nav.js
+++ src/js/components/Dashboard/Nav.js
@@ -1,25 +1,44 @@
 import React from 'react';
-import { Link } from 'react-router';
-
+import Anchor from 'grommet/components/Anchor';
+import Box from 'grommet/components/Box';
 import Header from 'grommet/components/Header';
-import Title from 'grommet/components/Title';
+import Heading from 'grommet/components/Heading';
 import Menu from 'grommet/components/Menu';
+import Image from 'grommet/components/Image';
 
-export default function Nav(props) {
+const CLASS_ROOT = "grommet-cms-header";
+
+export default function Nav({ onLogoutClick }) {
   return (
-    <Header className="dashboard__nav" justify="between" pad="medium">
-      <Title>
+    <Header
+      className={CLASS_ROOT} 
+      justify="between" 
+      pad={{ horizontal: 'medium', vertical: 'none' }}
+      colorIndex="accent-2"
+      align="center"
+      size="small">
+      <Heading tag="h4" strong={true} margin="none">
         Grommet CMS Dashboard
-      </Title>
+      </Heading>
 
-      <Menu label="Menu" inline={true} direction="row">
-        <Menu label="Pages">
-          <Link to="/dashboard/homepage">Home Page</Link>
-          <Link to="/dashboard/press-releases">Press Releases</Link>
+      <Box direction="row" responsive={false} align="center"
+        pad={{ between: 'medium' }}>
+        <Menu label="Menu" inline={true} direction="row">
+          <Anchor path="/dashboard/homepage" label="Home Page" />
+          <Anchor path="/dashboard/press-releases" label="Press Releases" />
+          <Anchor path="/dashboard/users" label="Users" />
+          <Anchor onClick={onLogoutClick} label="Sign Out" />
         </Menu>
-        <Link to="/dashboard/users">Users</Link>
-        <a onClick={props.onLogoutClick} >Sign Out</a>
-      </Menu>
+
+        <Image 
+          src="/img/dashboard/user-thumb.jpg" 
+          style={{
+            borderRadius: 25,
+            width: '25px',
+            height: '25px'
+          }}
+        />
+      </Box>
     </Header>
   );
 };
