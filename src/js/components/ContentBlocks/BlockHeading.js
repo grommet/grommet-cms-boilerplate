429da816b330ff34142f7e0fc9b50f67d87452bd
karatechops <contact@lazers.tv>
2017-01-09 16:29:37 -0500

    Update content blocks to be more generic.

--- src/js/components/ContentBlocks/BlockHeading.js
+++ src/js/components/ContentBlocks/BlockHeading.js
@@ -4,7 +4,7 @@ import Section from 'grommet/components/Section';
 
 export default function BlockHeading ({ content }) {
   return (
-    <Section className="labs__section" pad={{ horizontal: 'large', vertical: 'small' }}
+    <Section pad={{ horizontal: 'large', vertical: 'small' }} 
       alignSelf="center">
       <Heading tag="h2" margin="none">
         {content}
--- src/js/components/ContentBlocks/BlockQuote.js
+++ src/js/components/ContentBlocks/BlockQuote.js
@@ -1,15 +1,20 @@
 import React, { PropTypes } from 'react';
 import Box from 'grommet/components/Box';
-import LabsQuote from '../LabsQuote';
+import Heading from 'grommet/components/Heading';
+import Quote from 'grommet/components/Quote';
 
 export default function BlockQuote ({ content, source }) {
   return (
     <Box full="horizontal" colorIndex="light-1" align="center">
       <Box pad="small">
-        <LabsQuote 
-          quote={content}
-          source={source}
-        />
+        <Quote borderColorIndex="brand" pad="large">
+          <Heading tag="h2" strong={true}>
+            <span className="quote__open">“</span>
+            {content}
+            <span className="quote__close">”</span>
+          </Heading>
+          {source}
+        </Quote>
       </Box>
     </Box>
   );
