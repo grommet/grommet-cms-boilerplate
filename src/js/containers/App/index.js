28ff8cb59e5dfe199141d995db24c97e2fd489f5
Ryan Collins <rccproductions@mac.com>
2017-01-09 15:34:01 -0500

    Feat: integrate flow with app container

--- src/js/containers/App/index.js
+++ src/js/containers/App/index.js
@@ -1,10 +1,15 @@
+// @flow
 // (C) Copyright 2014-2015 Hewlett Packard Enterprise Development LP
-
 import React from 'react';
 import GrommetApp from 'grommet/components/App';
 import Helmet from 'react-helmet';
 
+type Props = {
+  children: React$Element,
+};
+
 class App extends React.Component {
+  props: Props;
   render() {
     return (
       <GrommetApp centered={false}>
