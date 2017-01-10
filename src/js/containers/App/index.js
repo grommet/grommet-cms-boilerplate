30877fe406f71adb21146577efd9522bca8042ef
Ryan Collins <rccproductions@mac.com>
2017-01-10 09:48:16 -0500

    Fix: revert app and pressrelease page to master after removing example app

--- src/js/containers/App/index.js
+++ src/js/containers/App/index.js
@@ -1,4 +1,5 @@
 // (C) Copyright 2014-2015 Hewlett Packard Enterprise Development LP
+
 import React from 'react';
 import GrommetApp from 'grommet/components/App';
 import Helmet from 'react-helmet';
