83d2834be506862e3543f932188ffeae2897b8dd
Ryan Collins <rccproductions@mac.com>
2017-01-13 12:52:42 -0500

    Feat: add mocks for PostPreview snapshot tests

---
+++ src/js/components/PostPreview/tests/__mocks__/props.js
@@ -0,0 +1,26 @@
+export default {
+  "post": {
+    "_id": "58782560aee165272b34614e",
+    "title": "Watch our Discover Video",
+    "date": "2017-01-12T05:00:00.000Z",
+    "slug": "watch-our-discover-video",
+    "image": "/uploads/media/2017/1/labs-blogfeed-1484268872525.gif",
+    "__v": 1,
+    "createdAt": "2017-01-13T17:39:39.496Z",
+    "contentBlocks": [
+      {
+        "card": {
+          "linkUrl": "https://community.hpe.com/t5/Behind-the-scenes-Labs/Watch-our-Discover-video-on-the-journey-toward-exascale/ba-p/6929821",
+          "linkText": "Read More",
+          "label": "Blog Post",
+          "heading": "Watch our Discover video on the journey toward exascale computing (VIDEO)"
+        },
+        "image": "/uploads/media/2017/1/labs-blogfeed-1484268872525.gif",
+        "content": "",
+        "blockType": "BlockCardParagraph",
+        "edit": false,
+        "id": "3b9edb86-519a-4603-99f7-f14c9d78df12"
+      }
+    ]
+  }
+};
--- src/js/components/PostPreview/tests/index.test.js
+++ src/js/components/PostPreview/tests/index.test.js
@@ -2,11 +2,14 @@ import { shallow } from 'enzyme';
 import { shallowToJson } from 'enzyme-to-json';
 import React from 'react';
 import PostPreview from '../index';
+import props from './__mocks__/props';
 
 describe('<PostPreview />', () => {
   it('should render with default props', () => {
     const wrapper = shallow(
-      <PostPreview />
+      <PostPreview
+        {...props}
+      />
     );
     expect(shallowToJson(wrapper)).toMatchSnapshot();
   });
