51301f2e630d797cf0e93a88f7eca7274dc38e04
Alex Mejias <contact@lazers.tv>
2017-01-15 12:23:51 -0500

    Remove comma dangles from functors.

--- src/js/utils/functors.js
+++ src/js/utils/functors.js
@@ -2,34 +2,34 @@ export default class Functors {
   static Identity = x => ({
     map: f => Functors.Identity(f(x)),
     fold: f => f(x),
-    inspect: () => `Functors.Identity(${x})`,
+    inspect: () => `Functors.Identity(${x})`
   });
   static Right = x => ({
     map: f => Functors.Right(f(x)),
     fold: (_, g) => g(x),
-    inspect: () => `Functors.Right(${x})`,
+    inspect: () => `Functors.Right(${x})`
   });
   static Left = x => ({
     map: () => Functors.Left(x),
     fold: f => f(x),
-    inspect: () => `Functors.Left(${x})`,
+    inspect: () => `Functors.Left(${x})`
   });
   static Sum = x => ({
     x,
     concat: ({ x: y }) =>
       Functors.Sum(x + y),
-    inspect: () => `Functors.Sum(${x})`,
+    inspect: () => `Functors.Sum(${x})`
   });
   static All = x => ({
     x,
     concat: ({ x: y }) =>
       Functors.All(x && y),
-    inspect: () => `Functors.All(${x})`,
+    inspect: () => `Functors.All(${x})`
   });
   static First = x => ({
     x,
     concat: () =>
       Functors.First(x),
-    inspect: () => `Functors.First(${x})`,
+    inspect: () => `Functors.First(${x})`
   });
 }
