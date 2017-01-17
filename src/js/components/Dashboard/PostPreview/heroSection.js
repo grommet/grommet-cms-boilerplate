fb92546060958d3f4f67ab46dbf3e8ba0c910784
Ryan Collins <rccproductions@mac.com>
2017-01-17 17:51:46 -0500

    Feat: refactor post preview hero section

---
+++ src/js/components/Dashboard/PostPreview/heroSection.js
@@ -0,0 +1,58 @@
+/* @flow */
+import React from 'react';
+import Hero from 'grommet/components/Hero';
+import Image from 'grommet/components/Image';
+import Box from 'grommet/components/Box';
+import Headline from 'grommet/components/Headline';
+import Label from 'grommet/components/Label';
+import Section from 'grommet/components/Section';
+// $FlowFixMe required module not found. See here: https://github.com/facebook/flow/issues/101
+import { ContentBlocks } from 'grommet-cms/containers';
+import type ContentBlockType from './flowTypes';
+// $FlowFixMe required module not found. See here: https://github.com/facebook/flow/issues/101
+import type { Asset } from 'grommet-cms/containers/Assets/flowTypes';
+
+export default function PostPreviewHeroSection(props: {
+  post?: {
+    image: Asset,
+    title: string,
+    subtitle?: string,
+    sections?: Array<{
+      contentBlocks: Array<ContentBlockType>
+    }>
+  },
+  selectedSection?: number
+}) {
+  const { post, selectedSection } = props;
+  if (post && (selectedSection == null || selectedSection === 0)) {
+    return (
+      <Box>
+        <Hero
+          className="post-preview--hero"
+          size="medium"
+          colorIndex="grey-2-a"
+          background={post.image ? <Image src={post.image.path} fit="cover" /> : null}
+        >
+          <Box direction="row" pad="medium">
+            {post.title &&
+              <Box colorIndex="grey-2-a" basis="full" pad="medium">
+                <Headline size="medium" strong className="post-preview--hero__headline">
+                  {post.title}
+                </Headline>
+                <Label truncate uppercase>
+                  {post.subtitle || ''}
+                </Label>
+              </Box>
+            }
+          </Box>
+        </Hero>
+        {post.sections.length && post.sections.map((item, i) =>
+          <Section key={i} pad="medium">
+            <ContentBlocks blocks={item.contentBlocks} />
+          </Section>  
+        )}
+      </Box>
+    );
+  }
+  return null;
+}
--- src/js/components/Dashboard/PostPreview/index.js
+++ src/js/components/Dashboard/PostPreview/index.js
@@ -1,16 +1,13 @@
 /* @flow */
 import React from 'react';
-import Hero from 'grommet/components/Hero';
-import Image from 'grommet/components/Image';
 import Box from 'grommet/components/Box';
-import Headline from 'grommet/components/Headline';
-import Label from 'grommet/components/Label';
 import Section from 'grommet/components/Section';
 // $FlowFixMe required module not found. See here: https://github.com/facebook/flow/issues/101
 import { ContentBlocks } from 'grommet-cms/containers';
 import type ContentBlockType from './flowTypes';
 // $FlowFixMe required module not found. See here: https://github.com/facebook/flow/issues/101
 import type { Asset } from 'grommet-cms/containers/Assets/flowTypes';
+import PostPreviewHeroSection from './heroSection';
 
 export default function PostPreview(props: {
   post?: {
@@ -24,38 +21,20 @@ export default function PostPreview(props: {
   selectedSection?: number
 }) {
   const { post, selectedSection } = props;
+  const renderPostSections = selectedSection && selectedSection > 0;
   return (
     <Box>
-      {post && selectedSection == null && 
-        <Box>
-          <Hero
-            className="post-preview--hero"
-            size="medium"
-            colorIndex="grey-2-a"
-            background={post.image ? <Image src={post.image.path} fit="cover" /> : null}
-          >
-            <Box direction="row" pad="medium">
-              {post.title &&
-                <Box colorIndex="grey-2-a" basis="full" pad="medium">
-                  <Headline size="medium" strong className="post-preview--hero__headline">
-                    {post.title}
-                  </Headline>
-                  <Label truncate uppercase>
-                    {post.subtitle || ''}
-                  </Label>
-                </Box>
-              }
-            </Box>
-          </Hero>
-        </Box>
-      }
-      {post && post.sections &&
-        post.sections
+      <PostPreviewHeroSection
+        post={post}
+        selectedSection={selectedSection}
+      />
+      {renderPostSections && post &&
+        post.sections && post.sections
           .filter((_, i) => selectedSection === i)
           .map((item, i) => 
             <Section key={i} pad="medium">
               <ContentBlocks blocks={item.contentBlocks} />
-            </Section>  
+            </Section>
           )
       }
     </Box>
