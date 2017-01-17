f1fd86526943964243c28c4c7cc70fe11209e75b
Ryan Collins <rccproductions@mac.com>
2017-01-17 14:23:05 -0500

    Feat: refactor the post list item detail with old content block section

--- src/js/components/Dashboard/PostListItemDetail/index.js
+++ src/js/components/Dashboard/PostListItemDetail/index.js
@@ -1,6 +1,10 @@
 /* @flow */
 import React from 'react';
 import Box from 'grommet/components/Box';
+import Section from 'grommet/components/Section';
+import Footer from 'grommet/components/Footer';
+import Menu from 'grommet/components/Menu';
+import Button from 'grommet/components/Button';
 // import MarqueeForm from 'grommet-cms/components';
 // $FlowFixMe
 import { DashboardContentBlocks } from 'grommet-cms/containers';
@@ -13,13 +17,39 @@ export default function PostListItemDetail(props: {
     id: string,
     name: string,
     contentBlocks: Array<{}>
-  }
+  },
+  onSubmit: Function,
+  onCreateBlockClick: Function
 }) {
-  const { item } = props;
+  const { item, onSubmit, onCreateBlockClick } = props;
   return (
     <Box>
       <PageHeader title={`Edit ${item.name}`} />
-      <DashboardContentBlocks contentBlocks={item.contentBlocks} />
+      <Section pad="medium">
+        <Box pad="small">
+          <DashboardContentBlocks blocks={item.contentBlocks} />
+          <Footer align="center" justify="center" pad="large">
+            <Menu
+              className="dashboard--content-blocks__button-footer"
+              direction="row"
+              inline
+              responsive={false}
+            >
+              <Button
+                label="submit"
+                onClick={onSubmit}
+                primary={true}
+                type="submit"
+              />
+              <Button
+                label="add block"
+                onClick={onCreateBlockClick}
+                primary={false}
+              />
+            </Menu>
+          </Footer>
+        </Box>
+      </Section>
     </Box>
   );
 }
