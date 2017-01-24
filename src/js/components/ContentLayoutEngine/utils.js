cd8404379f044c5893d4ab7669bb11a1fef20de6
Ryan Collins <rccproductions@mac.com>
2017-01-24 16:12:52 -0500

    Feat: parse boolean strings in content layout engine

--- src/js/components/ContentLayoutEngine/utils.js
+++ src/js/components/ContentLayoutEngine/utils.js
@@ -1,7 +1,14 @@
+const parseBooleans = (str) => {
+  if (str === 'true' || str === 'false') {
+    return new Boolean(str);
+  };
+  return str;
+};
+
 export default (layout) =>
   layout ? Object.assign({}, ...layout.map((item, i) => 
     ({
-      [`${item.name}`]: item.value
+      [`${item.name}`]: parseBooleans(item.value)
     })
   ))
   : null;
